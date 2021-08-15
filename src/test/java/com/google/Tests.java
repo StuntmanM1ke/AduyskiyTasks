b00aea1ed0b51e20bdad4d8fc84980e6718ce496
StuntmanM1ke <88888304+StuntmanM1ke@users.noreply.github.com>
2021-08-15 13:33:40 +0300

    test

---
+++ src/test/java/com/google/Tests.java
@@ -0,0 +1,17 @@
+package com.google;
+
+import org.junit.jupiter.api.Test;
+
+public class Tests extends BaseTest{
+
+
+    @Test
+    public void firstTest(){
+        chromeDriver.get("https://www.google.com/");
+    }
+
+    @Test
+    public void wikiCheckTest() {
+
+    }
+}
